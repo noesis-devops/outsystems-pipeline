4df9eff1a8ffb9b06d9a0a0c3771199e1db4476b
rodrigoalcaide <148088891+rodrigoalcaide@users.noreply.github.com>
2025-03-19 17:00:51 +0000

    Update request.py

--- request.py
+++ request.py
@@ -91,7 +91,8 @@ def create_deployment_plan(processed_issues, outsystems_url, lifetime_token, sou
         '-t', lifetime_token,
         '-s', source_env,
         '-d', target_env,
-        '-l', applications_json
+        '-l', applications_json,
+        '--force'
     ]
     
     # Execute the deployment command
