7483fceb0fb73678d614bd31f47712ccc329bffc
rodrigoalcaide <148088891+rodrigoalcaide@users.noreply.github.com>
2025-03-21 10:57:33 +0000

    Update request.py - try with manifest v2

--- request.py
+++ request.py
@@ -91,7 +91,7 @@ def create_deployment_plan(processed_issues, outsystems_url, lifetime_token, sou
         '-t', lifetime_token,
         '-s', source_env,
         '-d', target_env,
-        '-l', applications_json
+        '-m', applications_json
     ]
     
     # Execute the deployment command
